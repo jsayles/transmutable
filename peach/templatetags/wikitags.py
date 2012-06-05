3545bf7771e356d2970cbed12e4c3e61ae67507f
Trevor F. Smith <trevor@trevor.smith.name>
2012-06-04 17:32:00 -0700

    Start using safe markdown and add rel=nofollow to user contributed links.

--- peach/templatetags/wikitags.py
+++ peach/templatetags/wikitags.py
@@ -1,15 +1,17 @@
 # Copyright 2009,2010 GORBET + BANERJEE (http://www.gorbetbanerjee.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 import re
+import markdown
 from django import template
 from django.core.urlresolvers import reverse
 from django.utils.html import strip_tags, linebreaks, urlize
-from django.contrib.markup.templatetags.markup import markdown
+
 register = template.Library()
 
 @register.filter
 def wiki(text, namespace):
 	"""Convert the text into HTML using markdown and image name replacement."""
-	text = markdown(text)
+	md = markdown.Markdown(safe_mode="escape", extensions=['nofollow'])
+	text = md.convert(text)
 	return text
 
 @register.filter
@@ -43,4 +45,3 @@ def truncate(value, arg):
 		return value[:length] + "..."
 	else:
 		return value
-
